68831a29adf6efa744be5b36b24c60a6878b16fc
Jean luc <you@example.com>
2016-12-22 21:44:38 +0100

    synchro

--- source/Debian/Task.php
+++ source/Debian/Task.php
@@ -6,38 +6,9 @@ namespace Phansible\Debian;
 
 
 
-class Task extends \Phansible\Task
+class Task extends \Phansible\Module\Apt
 {
 
-
-	public function install($name, $package, $version='latest') {
-
-		return $this->createAction($name, array(
-			'apt'=>array(
-				'name'=>$package,
-				'state'=>$version
-			)
-		), true);
-	}
-
-	public function upgradeAll() {
-
-		$this->createAction('apt-upgrade', array(
-			'apt'=>array(
-				'upgrade'=>'dist'
-			)
-		));
-	}
-
-
-	public function updateAll() {
-
-		$this->createAction('apt-update', array(
-			'apt'=>array(
-				'update_cache'=>'yes'
-			)
-		));
-	}
 
 
 }
---
+++ source/Module/Apt.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Phansible\Module;
+
+
+
+
+class Apt extends \Phansible\Task
+{
+
+
+
+
+	public function install($name, $package, $version='latest') {
+
+		return $this->createAction($name, array(
+			'apt'=>array(
+				'name'=>$package,
+				'state'=>$version
+			)
+		), true);
+	}
+
+	public function upgradeAll() {
+
+		$this->createAction('apt-upgrade', array(
+			'apt'=>array(
+				'upgrade'=>'dist'
+			)
+		));
+	}
+
+
+	public function updateAll() {
+
+		$this->createAction('apt-update', array(
+			'apt'=>array(
+				'update_cache'=>'yes'
+			)
+		));
+	}
+
+
+}
+
